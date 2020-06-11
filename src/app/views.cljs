34f8839f58138eafa5f0c5651606fe827567bed7
marcelllaks <marcelllakos@gmail.com>
2020-06-11 17:31:22 +0200

    az also kartyak nem bugolnak szet, de meg el vannak csuszva

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -4,7 +4,7 @@
 
 (defn app []
   (reagent/create-class
-    {:component-did-mount (fn [this] (new Swiper ".swiper-container", (clj->js {:slidesPerView "5" :centeredSlides "false" :spaceBetween "0"})))
+    {:component-did-mount (fn [this] (new Swiper ".swiper-container", (clj->js {:slidesPerView "5" :centeredSlides "false" :spaceBetween "0" :pagination {:el ".swiper-pagination"} :navigation {:nextEl ".swiper-button-next" :prevEl ".swiper-button-prev"} :scrollbar {:el ".swiper-scrollbar"}})))
      :reagent-render
      (fn []
       [:div.custom-homepage-wrapper
@@ -64,15 +64,17 @@
              [:h4.card-title "EDUCATION & MENTORING"]
              [:div.card-desc-box
               [:p "We organize exclusive programming courses for free on a weekly basis.."]
-              [:p "One of our main goals with ZGEN is to create a platform where we fund projects of talented guild members and help them bootstrap their ventures."]]]]]]]]
-       [:div.container-z.last-cont
-        [:div.subtitle-wrapper
-         [:h3.leadership-title "THE COUNCIL"]
-         [:p
-          [:b "Rule 2:"]
-          "Leadership should include generation Z members only."]]
+              [:p "One of our main goals with ZGEN is to create a platform where we fund projects of talented guild members and help them bootstrap their ventures."]]]]]]]
+        [:div.container-z.last-cont
+         [:div.subtitle-wrapper
+          [:h3.leadership-title "THE COUNCIL"]
+          [:p
+           [:b "Rule 2:"]
+           "Leadership should include generation Z members only."]]]]
+       [:div
         [:div.swiper-container
          [:div.swiper-wrapper
+
           [:div.swiper-slide
            [:div.work-card
             [:img.bruno {:src "https://zgen.hu/uploads/default/original/1X/f523474b2548ab1f52a181e69046e2b457fd9992.jpg"}]
@@ -83,14 +85,14 @@
                 [:p "Bruno is the all seeing eye of ZGEN and its associates, his natural leader abilities and consistency make him a perfect Chief Operating Officer."]
                 [:p "He is the co-founder of "
                  [:a {:href "https://sulibot.hu" :target "_blank" :style {:color "black" :font-weight "800"}} "Sulibot"]
-                 ", a popular educational chatbot solution."]]]]]
-            [:div.card-name
-             [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/zawiasa.bruno"}
-              [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-facebook"}]]]
-             [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/bruno-z-411656141/"}
-              [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-linkein"}]]]]]]
+                 ", a popular educational chatbot solution."]]]]
+             [:div.card-name
+              [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/zawiasa.bruno"}
+               [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-facebook"}]]]
+              [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/bruno-z-411656141/"}
+               [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-linkein"}]]]]]]]
           [:div.swiper-slide
            [:div.work-card
             [:img.berci {:src "https://zgen.hu/uploads/default/original/1X/cf2f5695e44a32256d0503433862116004be9247.jpg"}]
@@ -100,11 +102,11 @@
                [:div.card-desc-box
                 [:p "Bernat is just as precise as his end products."]
                 [:p "He could create robots from anything which qualifies him as our Chief Technology Officer."]
-                [:p "When he doesn't do that, he plays darts."]]]]]
-            [:div.card-name
-             [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/bernat.zawiasa"}
-              [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-facebook"}]]]]]]
+                [:p "When he doesn't do that, he plays darts."]]]]
+             [:div.card-name
+              [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/bernat.zawiasa"}
+               [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-facebook"}]]]]]]]
           [:div.swiper-slide
            [:div.work-card
             [:img.samu {:src "https://zgen.hu/uploads/default/original/1X/7d45caa7275933223c1c8a6335784875c63cd81a.jpg"}]
@@ -115,14 +117,14 @@
                 [:p "Samuel has unmatched abilities to read, understand and learn things."]
                 [:p "He is an organizer of "
                  [:a {:href "https://www.facebook.com/mccdebate/" :target "_blank" :style {:color "black" :font-weight "800"}} "MCC Debate"]]
-                [:p "also has written for prominent hungarian newspapers since his early years in university."]]]]]
-            [:div.card-name
-             [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/samuel.kallo.49"}
-              [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-facebook"}]]]
-             [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/s%C3%A1muel-k%C3%A1ll%C3%B3-975428152/"}
-              [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-linkein"}]]]]]]
+                [:p "also has written for prominent hungarian newspapers since his early years in university."]]]]
+             [:div.card-name
+              [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/samuel.kallo.49"}
+               [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-facebook"}]]]
+              [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/s%C3%A1muel-k%C3%A1ll%C3%B3-975428152/"}
+               [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-linkein"}]]]]]]]
           [:div.swiper-slide
            [:div.work-card
             [:img.aron {:src "https://zgen.hu/uploads/default/original/1X/f624aace4d9974b95ff6f66aaee4d14fdbb8c5ee.jpg"}]
@@ -131,14 +133,14 @@
               [:h4.card-title "INVESTMENT"
                [:div.card-desc-box
                 [:p "Robert made his way into the investment world early in his career as an early adopter of cryptocurrencies."]
-                [:p "He is responsible for company strategy and investor relationships as the Chief Investment Officer."]]]]]
-            [:div.card-name
-             [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/aaronpowered"}
-              [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-facebook"}]]]
-             [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/zawiasa/"}
-              [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-linkein"}]]]
-             [:a.social-link.twitter {:title "" :target "_blank" :href "https://twitter.com/aaronpowered"}
-              [:svg.fa.d-icon-fab-twitter.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
-               [:use {:xlink:href "#fab-twitter"}]]]]]]]]]])}))
+                [:p "He is responsible for company strategy and investor relationships as the Chief Investment Officer."]]]]
+             [:div.card-name
+              [:a.social-link.facebook {:title "" :target "_blank" :href "https://www.facebook.com/aaronpowered"}
+               [:svg.fa.d-icon-fab-facebook.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-facebook"}]]]
+              [:a.social-link.linkedin {:title "" :target "_blank" :href "https://www.linkedin.com/in/zawiasa/"}
+               [:svg.fa.d-icon-fab-linkedin.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-linkein"}]]]
+              [:a.social-link.twitter {:title "" :target "_blank" :href "https://twitter.com/aaronpowered"}
+               [:svg.fa.d-icon-fab-twitter.svg-icon.svg-string {:xmlns "http://www.w3.org/2000/svg"}
+                [:use {:xlinkHref "#fab-twitter"}]]]]]]]]]]])}))
